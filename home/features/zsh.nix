1ac690c45efcd1aa1b58a4b5c4bfb2bf48fdefdf
Fukaya Temma <23367170+pranc1ngpegasus@users.noreply.github.com>
2023-09-17 01:30:35 +0900

    feat: zsh case insensitive completion (#21)

--- home/features/zsh.nix
+++ home/features/zsh.nix
@@ -2,7 +2,11 @@
   programs.zsh = {
     enable = true;
     defaultKeymap = "emacs";
+    enableCompletion = true;
     enableAutosuggestions = true;
+    initExtra = ''
+      zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
+    '';
     plugins = [
       {
         name = "zsh-ghq-fzf";
