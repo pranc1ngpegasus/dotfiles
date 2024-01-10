c527cdebf8d3ccdf7227d37851bb243108456013
Fukaya Temma <23367170+pranc1ngpegasus@users.noreply.github.com>
2024-01-10 22:26:56 +0900

    feat: setup useful cli (#43)

--- home/features/zsh.nix
+++ home/features/zsh.nix
@@ -9,6 +9,7 @@
       export PATH="$BUN_INSTALL/bin:$PATH"
       export PATH="$HOME/go/bin:$PATH"
       eval "$(fnm env --use-on-cd)"
+      eval $(/opt/homebrew/bin/brew shellenv)
       zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
     '';
     plugins = [
