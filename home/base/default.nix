bc3a57d1d48d90b90e5051c6d1cf294e543be58f
Pranc1ngPegasus <ride.or.die.2215@icloud.com>
2024-04-01 18:46:02 +0900

    fix: cleanup cli directories

---
+++ home/base/default.nix
@@ -0,0 +1,8 @@
+{...}: {
+  imports = [
+    ./editor
+    ./programs
+    ./terminal
+    ./shell
+  ];
+}
--- home/darwin/default.nix
+++ home/darwin/default.nix
@@ -1,16 +1,6 @@
 {config, ...}: {
   imports = [
-    ../features/bat.nix
-    ../features/cli.nix
-    ../features/fzf.nix
-    ../features/git.nix
-    ../features/gitui.nix
-    ../features/lazygit.nix
-    ../features/neovim
-    ../features/starship.nix
-    ../features/tmux.nix
-    ../features/wezterm.nix
-    ../features/zsh.nix
+    ../base
   ];
 
   home = {
