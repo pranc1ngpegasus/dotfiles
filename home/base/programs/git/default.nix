5620376641ba0e8c87abeaa8871abc3310bf27aa
Pranc1ngPegasus <ride.or.die.2215@icloud.com>
2024-07-31 18:06:50 +0900

    feat: sign git commit with gpg

--- home/base/programs/git/default.nix
+++ home/base/programs/git/default.nix
@@ -1,4 +1,13 @@
-{config, ...}: {
+{
+  pkgs,
+  config,
+  ...
+}: let
+  gpg_ssh_program =
+    if pkgs.system == "aarch64-darwin"
+    then "/Applications/1Password.app/Contents/MacOS/op-ssh-sign"
+    else "";
+in {
   home = {
     sessionVariables = {
       GHQ_SELECTOR = "fzf-tmux";
@@ -13,6 +22,15 @@
       core = {
         editor = "nvim";
       };
+      commit = {
+        gpgsign = true;
+      };
+      gpg = {
+        format = "ssh";
+        "ssh" = {
+          program = gpg_ssh_program;
+        };
+      };
       push = {
         default = "current";
       };
@@ -26,6 +44,9 @@
       };
       ghq = {
         root = config.home.homeDirectory + "/ghq";
+      };
+      user = {
+        signingkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPrxAPvOxoy8a5y3hp9iKTWGyk+qgBTYv8DgfTnqQR8/";
       };
     };
     ignores = [
