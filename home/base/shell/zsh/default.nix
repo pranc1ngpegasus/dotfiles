88e273d98565779eb430cbfae0e587163598a533
Fukaya Temma <23367170+pranc1ngpegasus@users.noreply.github.com>
2024-07-31 22:02:20 +0900

    Revert "feat: install homebrew via nix (#71)" (#72)
    This reverts commit acd89f8a550b500b0e1e28239a27935a62d0696d.

--- home/base/shell/zsh/default.nix
+++ home/base/shell/zsh/default.nix
@@ -11,7 +11,9 @@
       export PATH="$BUN_INSTALL/bin:$PATH"
       export PATH="$HOME/go/bin:$PATH"
       eval "$(fnm env --use-on-cd)"
+      eval $(/opt/homebrew/bin/brew shellenv)
       zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
+      eval "$(/opt/homebrew/bin/brew shellenv)"
     '';
     plugins = [
       {
