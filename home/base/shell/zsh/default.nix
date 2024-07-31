f1cd08d067b4c435e7f2b101f8163f835fcad83c
Pranc1ngPegasus <ride.or.die.2215@icloud.com>
2024-07-31 18:06:15 +0900

    feat: install homebrew via nix

--- home/base/shell/zsh/default.nix
+++ home/base/shell/zsh/default.nix
@@ -11,9 +11,7 @@
       export PATH="$BUN_INSTALL/bin:$PATH"
       export PATH="$HOME/go/bin:$PATH"
       eval "$(fnm env --use-on-cd)"
-      eval $(/opt/homebrew/bin/brew shellenv)
       zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
-      eval "$(/opt/homebrew/bin/brew shellenv)"
     '';
     plugins = [
       {
