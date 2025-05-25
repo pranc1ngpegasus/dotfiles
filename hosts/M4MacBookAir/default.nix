5425cfb1dd98a345423f0f2543a59a5505655e99
pranc1ngpegasus <temma.fukaya@mokmok.dev>
2025-05-25 20:44:38 +0900

    feat: set system.primaryUser

--- hosts/M4MacBookAir/default.nix
+++ hosts/M4MacBookAir/default.nix
@@ -14,6 +14,10 @@
     hostName = "M4MacBookAir";
   };
 
+  system = {
+    primaryUser = "pranc1ngpegasus";
+  };
+
   users.users.pranc1ngpegasus = {
     name = "pranc1ngpegasus";
     home = "/Users/pranc1ngpegasus";
