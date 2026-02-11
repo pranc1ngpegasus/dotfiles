8b24d6890209e13ce28da9f08a80816abee2b6a0
pranc1ngpegasus <temma.fukaya@mokmok.dev>
2026-02-12 00:05:03 +0900

    pin zsh plugin revs to commit sha

--- home/base/shell/zsh/default.nix
+++ home/base/shell/zsh/default.nix
@@ -34,7 +34,7 @@
         src = pkgs.fetchFromGitHub {
           owner = "pranc1ngpegasus";
           repo = "zsh-ghq-skim";
-          rev = "main";
+          rev = "5d030e7aef94eb1804a0f216a801c47bd357d2a4";
           sha256 = "sha256-U/b7zdP5y1k5kADNy7hf7BYr9H7AZsL6arW1qOozdxk=";
         };
       }
@@ -43,7 +43,7 @@
         src = pkgs.fetchFromGitHub {
           owner = "pranc1ngpegasus";
           repo = "zsh-gwt-skim";
-          rev = "main";
+          rev = "1cb2450bfa74e31194503805d9cec22711aa7fa4";
           sha256 = "sha256-gVdDnvEUG3JiuDDVWrDeOBlmF+C7KbQwNiIVDukT+xY=";
         };
       }
