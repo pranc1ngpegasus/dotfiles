b6385417eb6fa9fab139554292447c5aa96be7b5
pranc1ngpegasus <temma.fukaya@mokmok.dev>
2026-02-28 16:05:09 +0900

    Fix ssh match block options

--- home/base/programs/ssh/default.nix
+++ home/base/programs/ssh/default.nix
@@ -1,27 +1,14 @@
-{ pkgs, ... }:
-let
-  _1passwordSockPath =
-    if pkgs.stdenv.hostPlatform.system == "aarch64-darwin" then
-      "~/Library/Group\\ Containers/2BUA8C4S2C.com.1password/t/agent.sock"
-    else
-      "~/.1password/agent.sock";
-in
+{ ... }:
 {
   programs.ssh = {
     enable = true;
     enableDefaultConfig = false;
-    matchBlocks."*" = {
-      forwardAgent = false;
-      addKeysToAgent = "no";
-      compression = false;
-      serverAliveInterval = 0;
-      serverAliveCountMax = 3;
-      hashKnownHosts = false;
-      userKnownHostsFile = "~/.ssh/known_hosts";
-      controlMaster = "no";
-      controlPath = "~/.ssh/master-%r@%n:%p";
-      controlPersist = "no";
-      identityAgent = _1passwordSockPath;
+    matchBlocks."github.com/" = {
+      addKeysToAgent = "yes";
+      extraOptions = {
+        UseKeychain = "yes";
+      };
+      identityFile = "~/.ssh/id_ed25519";
     };
   };
 }
