e2f125c7da8ba92e444baa64eec3e052cdf15f0c
pranc1ngpegasus <temma.fukaya@mokmok.dev>
2026-07-15 21:54:09 +0900

    trim Neovim defaults and parsers

--- home/base/editor/neovim/default.nix
+++ home/base/editor/neovim/default.nix
@@ -60,19 +60,33 @@
           vim.keymap.set('n', '<C-f>', '<cmd>Telescope live_grep<CR>', { noremap = true, silent = true })
         '';
       }
-      nvim-treesitter.withAllGrammars
+      (nvim-treesitter.withPlugins (
+        plugins: with plugins; [
+          bash
+          go
+          gomod
+          gosum
+          gowork
+          javascript
+          json
+          lua
+          markdown
+          markdown_inline
+          nix
+          rust
+          toml
+          tsx
+          typescript
+          vim
+          vimdoc
+          yaml
+        ]
+      ))
       {
         plugin = gitlinker-nvim;
         type = "lua";
         config = ''
-          local gitlinker_group = vim.api.nvim_create_augroup("gitlinker_lazy_setup", { clear = true })
-          vim.api.nvim_create_autocmd({"BufReadPost", "BufNewFile"}, {
-            group = gitlinker_group,
-            once = true,
-            callback = function()
-              require("gitlinker").setup()
-            end,
-          })
+          require("gitlinker").setup()
         '';
       }
     ];
@@ -82,23 +96,17 @@
       end)
 
       -- options
-      vim.opt.autoread = true
-      vim.opt.background = "dark"
       vim.opt.clipboard = "unnamed"
       vim.opt.cmdheight = 0
       vim.opt.cursorcolumn = true
       vim.opt.cursorline = true
-      vim.opt.encoding = "UTF-8"
       vim.opt.expandtab = true
       vim.opt.ignorecase = true
       vim.opt.inccommand = "split"
-      vim.opt.incsearch = true
       vim.opt.laststatus = 3
-      vim.opt.backup = false
       vim.opt.showmode = false
       vim.opt.swapfile = false
       vim.opt.number = true
-      vim.opt.ruler = true
       vim.opt.scrolloff = 1000
       vim.opt.shiftround = true
       vim.opt.shiftwidth = 2
@@ -106,37 +114,10 @@
       vim.opt.smartindent = true
       vim.opt.tabstop = 2
       vim.opt.termguicolors = true
-      vim.opt.wrapscan = true
 
       -- completion
       vim.o.pumborder = "rounded"
       vim.opt.completeopt = { "menu", "menuone", "noselect", "fuzzy", "popup" }
-
-      -- disable unnecessary built-in plugins
-      vim.g.did_indent_on             = 1
-      vim.g.did_install_default_menus = 1
-      vim.g.did_install_syntax_menu   = 1
-      vim.g.did_load_ftplugin         = 1
-      vim.g.loaded_2html_plugin       = 1
-      vim.g.loaded_getscript          = 1
-      vim.g.loaded_getscriptPlugin    = 1
-      vim.g.loaded_gzip               = 1
-      vim.g.loaded_man                = 1
-      vim.g.loaded_matchit            = 1
-      vim.g.loaded_matchparen         = 1
-      vim.g.loaded_remote_plugins     = 1
-      vim.g.loaded_rrhelper           = 1
-      vim.g.loaded_shada_plugin       = 1
-      vim.g.loaded_spellfile_plugin   = 1
-      vim.g.loaded_sql_completion     = 1
-      vim.g.loaded_tar                = 1
-      vim.g.loaded_tarPlugin          = 1
-      vim.g.loaded_tutor_mode_plugin  = 1
-      vim.g.loaded_vimball            = 1
-      vim.g.loaded_vimballPlugin      = 1
-      vim.g.loaded_zip                = 1
-      vim.g.loaded_zipPlugin          = 1
-      vim.g.skip_loading_mswin        = 1
     '';
   };
 }
