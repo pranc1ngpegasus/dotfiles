656c1f936c5aa8e7f6ce0675b87b45c2dcf741c7
pranc1ngpegasus <temma.fukaya@mokmok.dev>
2026-07-19 22:37:29 +0900

    chore: simplify tmux

--- home/base/terminal/tmux/default.nix
+++ home/base/terminal/tmux/default.nix
@@ -11,36 +11,28 @@
     terminal = "tmux-256color";
     shell = "${pkgs.bashInteractive}/bin/bash";
     extraConfig = ''
-      set-option -ag terminal-overrides ',xterm-256color:RGB'
-      # ペインを均等幅に揃える
       bind-key = select-layout even-horizontal
-      # status line
-      ## common
+      set-option -ag terminal-overrides ',xterm-256color:RGB'
+      set-option -g allow-passthrough on
       set-option -g allow-rename on
+      set-option -g alternate-screen off
       set-option -g automatic-rename on
+      set-option -g extended-keys on
       set-option -g focus-events on
+      set-option -g pane-active-border-style "fg=#84a0c6"
+      set-option -g pane-border-style "fg=#1e2132"
       set-option -g renumber-windows on
-      set-option -g status-bg "colour237"
-      set-option -g status-fg "colour255"
       set-option -g status-interval 1
-      set-option -g status-justify "centre"
-      set-option -g status-position "bottom"
-      ## window status
-      set-option -g window-status-current-format " #I: #{pane_current_command} "
-      set-option -g window-status-current-style "fg=colour252,bg=colour27,bold"
-      set-option -g window-status-format " #I: #{pane_current_command} "
-      set-option -g window-status-style "fg=default,bg=default,bold"
-      ## window status left
-      set-option -g status-left "#[fg=colour252,bg=colour239] Session: #S #[default]"
-      set-option -g status-left-length 70
-      ## window status right
-      set-option -g status-right "#[fg=colour252,bg=colour239] %Y/%m/%d(%a) %H:%M:%S #[default]"
-      set-option -g status-right-length 70
-      ## pane border
-      set-option -g pane-active-border-style "fg=colour27,bg=default"
-      set-option -g pane-border-format " #P: #{pane_current_path} - #{pane_current_command} "
-      set-option -g pane-border-status top
-      set-option -g pane-border-style "fg=default,bg=default"
+      set-option -g status-left "#[fg=#c6c8d1] #h #[fg=#6b7089]#{b:pane_current_path} "
+      set-option -g status-left-length 40
+      set-option -g status-position bottom
+      set-option -g status-right "#[fg=#39ffb6,bold] %Y/%m/%d %H:%M "
+      set-option -g status-right-length 20
+      set-option -g status-style "fg=#444b71,bg=#0f1117"
+      set-option -g window-status-current-format " #I:#W "
+      set-option -g window-status-current-style "fg=#c6c8d1,bg=#1e2132,bold"
+      set-option -g window-status-format " #I:#W "
+      set-option -g window-status-style "fg=#444b71,bg=default"
     '';
     plugins = with pkgs; [
       tmuxPlugins.pain-control
