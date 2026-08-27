63399eba3d7c48fbd76b8f243e915c768019bc39
pranc1ngpegasus <temma.fukaya@mokmok.dev>
2026-08-27 11:24:59 +0900

    Remove delta git diff tool

--- home/base/programs/git.nix
+++ home/base/programs/git.nix
@@ -46,14 +46,4 @@
       ".zettelkasten/"
     ];
   };
-
-  programs.delta = {
-    enable = true;
-    enableGitIntegration = true;
-    options = {
-      navigate = true;
-      line-numbers = true;
-      side-by-side = false;
-    };
-  };
 }
