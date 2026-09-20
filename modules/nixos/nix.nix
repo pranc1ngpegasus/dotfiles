dacdf6a6b5f77696b98840f466ae0b2ccbdd5dff
pranc1ngpegasus <temma.fukaya@mokmok.dev>
2026-09-20 11:00:39 +0900

    nix: nix オプションの重複を解消する

--- modules/nixos/nix.nix
+++ modules/nixos/nix.nix
@@ -1,15 +1,17 @@
 {
   nixpkgs.config.allowUnfree = true;
 
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
+  nix = {
+    settings.experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
 
-  nix.settings.trusted-users = [ "root" ];
+    settings.trusted-users = [ "root" ];
 
-  # GC は determinate-nixd がディスク空き容量に応じて自動実行するため、ここでは有効化しない。
-  nix.optimise.automatic = true;
+    # GC は determinate-nixd がディスク空き容量に応じて自動実行するため、ここでは有効化しない。
+    optimise.automatic = true;
+  };
 
   time.timeZone = "Asia/Tokyo";
 }
